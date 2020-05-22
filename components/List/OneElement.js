071e20d61a3143db655c2822632e5f434a8e8ed5
Muhammad Tsaqif Ammar <54428874+tsaqifammar@users.noreply.github.com>
2020-05-22 14:44:52 +0700

    adding some styling

--- components/List/OneElement.js
+++ components/List/OneElement.js
@@ -2,14 +2,19 @@ import React from "react";
 
 const OneElement = (props) => (
 	<div>
-		<input
-			type="checkbox"
-			checked={props.todo.is_done}
-			onChange={(e) => props.toggleComplete(props.todo.id, e)}
-		/>
-		<p style={{ textDecoration: props.todo.is_done ? "line-through" : "" }}>
+		<label
+			style={{
+				textDecoration: props.todo.is_done ? "line-through" : "",
+			}}
+		>
+			<input
+				type="checkbox"
+				checked={props.todo.is_done}
+				onChange={(e) => props.toggleComplete(props.todo.id, e)}
+				style={{ width: "25px", height: "25px" }}
+			/>
 			{props.todo.task_name}
-		</p>
+		</label>
 	</div>
 );
 
